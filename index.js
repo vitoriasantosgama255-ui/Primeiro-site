1929f122a9461f38504c02c642b18474ab27c06d
vitoriasantosgama255-ui <vitoriasantosgama255@gmail.com>
2026-04-17 14:46:09 -0300

    finalizando o codigo

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ function mudar_conteudo_element(){
   document.getElementById("h1").innerHTML = "Hello Javascript!";
 
 };
+
+function mudar_estilo(){
+
+document.getElementById("paragrafo").style.textAlign = "right";
+}
